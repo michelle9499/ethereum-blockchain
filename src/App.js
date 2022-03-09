6758e411110b15d70906c828794ddb65dad15f9b
Michelle <michellewong9499@gmail.com>
2022-03-09 23:33:44 +0800

    Add get block details by hash

--- src/App.js
+++ src/App.js
@@ -12,6 +12,27 @@ class App extends React.Component {
     super();
     this.state = {
       latestBlockList: [],
+      blockDetails: {
+        number: undefined,
+        timestamp: '',
+        transactions: undefined,
+        miner: undefined,
+        blockReward: undefined,
+        unclesReward: undefined,
+        difficulty: undefined,
+        totalDifficulty: undefined,
+        size: undefined,
+        gasUsed: undefined,
+        gasLimit: undefined,
+        baseFeePerGas: undefined,
+        burntFees: undefined,
+        extraData: undefined,
+        hash: undefined,
+        parentHash: undefined,
+        sha3Uncles: undefined,
+        stateRoot: undefined,
+        nonce: undefined
+      }
     }
   }
 
@@ -57,20 +78,77 @@ class App extends React.Component {
   getLatestTransaction() {
     web3.eth.getBlockNumber()
       .then((n) => {
-        console.log(n)
+        // console.log(n);
         // web3.eth.getTransactionFromBlock(n.number)
         //   .then((t) => {
         //     console.log(t)
         //   });
         
       });
-    
-    
+  }
+  
+  //get block details by hash
+  getBlockDetailsByHash(hash){
+    web3.eth.getBlock(hash)
+      .then((r) => {
+        let dateTime = moment(r.timestamp*1000)
+
+        //base fee per gas
+        const baseFeePerGasToBN = web3.utils.toBN(r.baseFeePerGas)
+        const baseFeePerGasEther = web3.utils.fromWei(baseFeePerGasToBN, 'ether')
+        const baseFeePerGasGwei = web3.utils.fromWei(baseFeePerGasToBN, 'gwei')
+
+        //burnt fees
+        const burntFees = r.gasLimit*r.baseFeePerGas
+        const burntFeesToBN = web3.utils.toBN(burntFees)
+        const burntFeesToEther = web3.utils.fromWei(burntFeesToBN, 'ether')
+
+        //txn fee
+        const gasPrice = web3.eth.getGasPrice()
+        gasPrice.then((gp) => {
+          const txnFee = r.gasLimit * gp
+          const txnFeeToBN = web3.utils.toBN(txnFee)
+          const txnFeeToEther = web3.utils.fromWei(txnFeeToBN, 'ether')
+
+          //block rewards
+          const blockRewardToBN = web3.utils.toBN(2000000000000000000+txnFee-burntFees)
+          const brFromWei= web3.utils.fromWei(blockRewardToBN, 'ether')
+
+          web3.eth.getBlockTransactionCount(r.hash).then((transactionCount) => {
+            web3.eth.getBlockUncleCount(r.hash).then((uncleCount) => {
+              this.setState(prevState => {
+                let blockDetails = Object.assign({}, prevState.blockDetails);
+                blockDetails.number = r.number
+                blockDetails.timestamp = `${dateTime.fromNow()} (${dateTime.format('YYYY-MM-DD hh:mm:ss a')})`
+                blockDetails.transactions = `${transactionCount} transactions in this block`
+                blockDetails.miner = r.miner
+                blockDetails.blockReward = `${brFromWei} Ether (2 + ${txnFeeToEther} - ${burntFeesToEther})`
+                blockDetails.unclesReward = uncleCount
+                blockDetails.difficulty= r.difficulty
+                blockDetails.totalDifficulty= parseInt(r.totalDifficulty).toLocaleString()
+                blockDetails.size= `${r.size.toLocaleString()} bytes`
+                blockDetails.gasUsed= r.gasUsed.toLocaleString()
+                blockDetails.gasLimit= r.gasLimit.toLocaleString()
+                blockDetails.baseFeePerGas= `${baseFeePerGasEther} Ether (${baseFeePerGasGwei} Gwei)`
+                blockDetails.burntFees= `${burntFeesToEther} Ether`
+                blockDetails.extraData= r.extraData
+                blockDetails.hash= r.hash
+                blockDetails.parentHash= r.parentHash
+                blockDetails.sha3Uncles= r.sha3Uncles
+                blockDetails.stateRoot= r.stateRoot
+                blockDetails.nonce= r.nonce
+                return { blockDetails }
+              });
+            })
+          });
+        });
+      });
   }
 
   componentDidMount(){
     this.getLatestBlock();
     this.getLatestTransaction();
+    this.getBlockDetailsByHash('0x1b43a0e61c5f1cecf0513653e09935f7236bbc3f794ccedc592cd5a4a49c661f');
   }
 
   render() {
@@ -78,7 +156,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<EthereumComponent latestBlockList={this.state.latestBlockList}/>} />
-            <Route path="/details" element={<EthereumDetailsComponent />} />
+            <Route path="/details" element={<EthereumDetailsComponent blockDetails={this.state.blockDetails}/>} />
         </Routes>
       </BrowserRouter>
     )
--- src/ethereum/ethereum-details.component.js
+++ src/ethereum/ethereum-details.component.js
@@ -3,73 +3,73 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBarComponent from './nav-bar.component';
 import { Card } from 'react-bootstrap';
 
-const EthereumDetailsComponent = () => {
+const EthereumDetailsComponent = (props) => {
     return (
         <div>
             <NavBarComponent />
             <div className='container'>
                 <Card className='my-5'>
                     <Card.Header>
-                        <p className='mb-0'><span className='h3'>Block</span> <span className='h5 text-secondary'>#123456</span></p>
+                        <p className='mb-0'><span className='h3'>Block</span> <span className='h5 text-secondary'>#{props.blockDetails.number}</span></p>
                     </Card.Header>
                     <Card.Body>
                         <div className='row'>
                             <div className='col-3 mb-3'>Block Height:</div>
-                            <div className='col-9 mb-3'>123456</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.number}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Timestamp:</div>
-                            <div className='col-9 mb-3'>5 mins ago (Mar-09-2022 01:58:04 AM +UTC)</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.timestamp}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Transactions:</div>
-                            <div className='col-9 mb-3'>256 transactions and 189 contract internal transactions in this block</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.transactions}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Mined by:</div>
-                            <div className='col-9 mb-3'>0x829bd824b016326a401d083b33d092293333a830 (F2Pool Old) in 1 secs</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.miner}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Block Reward:</div>
-                            <div className='col-9 mb-3'>2.071390026072561117 Ether (2 + 0.887745958518115345 - 0.816355932445554228)</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.blockReward}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Uncles Reward:</div>
-                            <div className='col-9 mb-3'>0</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.unclesReward}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Difficulty:</div>
-                            <div className='col-9 mb-3'>12,491,022,224,873,051</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.difficulty}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Total Difficulty:</div>
-                            <div className='col-9 mb-3'>43,312,235,093,337,716,358,232</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.totalDifficulty}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Size:</div>
-                            <div className='col-9 mb-3'>147,709 bytes</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.size}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Gas Used:</div>
-                            <div className='col-9 mb-3'>23,986,249 (79.80%)</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.gasUsed}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Gas Limit:</div>
-                            <div className='col-9 mb-3'>30,058,590</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.gasLimit}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Base Fee Per Gas:</div>
-                            <div className='col-9 mb-3'>0.000000034034330772 Ether (34.034330772 Gwei)</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.baseFeePerGas}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Burnt Fees:</div>
-                            <div className='col-9 mb-3'>0.816355932445554228 Ether</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.burntFees}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Extra Data:</div>
-                            <div className='col-9 mb-3'>七彩神仙鱼H#! (Hex:0xe4b883e5bda9e7a59ee4bb99e9b1bc482321)</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.extraData}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Hash:</div>
-                            <div className='col-9 mb-3'>0xede98c617add97e41ca0436ceeeb40d8161f2ebb56e52561418cbac8c0390be6</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.hash}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Parent Hash:</div>
-                            <div className='col-9 mb-3'>0xfa4b9852ceb9f982455c2f2c109336f2fb10e1c6604ad6a56d2190c2158bcd93</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.parentHash}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Sha3Uncles:</div>
-                            <div className='col-9 mb-3'>0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.sha3Uncles}</div>
                             <hr/>
                             <div className='col-3 mb-3'>StateRoot:</div>
-                            <div className='col-9 mb-3'>0x319cb1bd5c04b02a5e9bc497eb62effb02e5959ac370cb20e978430bf9e3d75f</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.stateRoot}</div>
                             <hr/>
                             <div className='col-3 mb-3'>Nonce:</div>
-                            <div className='col-9 mb-3'>0xb170711f1b5f75ef</div>
+                            <div className='col-9 mb-3'>{props.blockDetails.nonce}</div>
                         </div>
                     </Card.Body>
                 </Card>
